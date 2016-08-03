b8392e4124e01f2c0d643ee3ef2468b066b49d11
mainbord <n56016gb@gmail.com>
2016-08-03 14:10:54 +0300

    ррр

---
+++ firstclass.java
@@ -0,0 +1,14 @@
+package main;
+
+/**
+ * Created by work on 07.10.2015.
+ */
+public class firstclass {
+    public static void main(String[] args){
+        System.out.println("Starting app...");
+
+        System.out.println("123");
+
+    }
+
+}
